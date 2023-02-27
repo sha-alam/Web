42d28fca8dc4a954a3058a58169aa0698a6379af
sha-alam <shaalam.ice@pust.ac.bd>
2023-02-28 01:08:47 +0600

    2nd

--- JAVASCRIPT/hello.js
+++
@@ -1,8 +0,0 @@
-var num1 = 20;
-var num2 = 10;
-
-var multiplication = num1 * num2;
-var division = num1 / num2;
-
-console.log("Multiplication: " + multiplication);
-console.log("Division: " + division);
